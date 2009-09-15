0f824413e4f06a61f7d97b480b1c6c2d2df8b597
dave <dave@mt64.local>
2009-09-14 21:35:28 -0700

    Not quite working, but I think I'm using Tim's stuff properly.

--- version.hs
+++ version.hs
@@ -6,29 +6,12 @@ import qualified Data.ByteString.Lazy.Char8 as C
 import Network.Socket hiding (send, recv)
 import Network.Socket.ByteString.Lazy
 import Data.Int
-import Data.Binary
+import Data.Binary hiding (get, put)
+import Data.Binary.Get
+import Data.Binary.Put
 import Data.Binary.Builder
 import Debug.Trace
-import Nine hiding (main)
-
-next s (Rversion _ _ _ _ _ _) = do
-  let tattachMessPayload = encode tattachClientMessage
-  putStrLn . show $ C.length tattachMessPayload
-  putStrLn $ printHex tattachMessPayload
-  send s (encode tattachClientMessage) >>= print . ("Sent: " ++) . show
-  print "Tattach message sent, getting response"
-  msg <- recv s 100
-  putStrLn $ printHex msg
-  return msg
-
-finalstep (Rattach _ _ _ q) = do
-  putStrLn "Rattach came back.. QID following"
-  print q
-
-finalstep (Rerror _ _ _ _ message) = do
-  putStrLn "Rerror returned"
-  print message
-
+import NineP
 
 main :: IO ()
 main = withSocketsDo $
@@ -36,19 +19,28 @@ main = withSocketsDo $
           ainfo <- getAddrInfo Nothing (Just "127.0.0.1") (Just "6872")
           let a = head ainfo
           sock <- socket AF_INET Stream defaultProtocol
+          putStrLn "Trying to connect"
           connect sock (addrAddress (traceShow a a))
-          let tversionMessPayload = encode tversionClientMessage
-          putStrLn . show $ C.length tversionMessPayload
-          putStrLn $ printHex tversionMessPayload
-          send sock (tversionMessPayload) >>= print . ("Sent: " ++) . show
-          print "Tversion Message sent, receiving response"
+          putStrLn "connected!"
+          let version = Msg TTversion (-1) $ Tversion 1024 "9P2000"
+          putStrLn $ "About to send: " ++ show version
+          send sock $ runPut (put version) 
           msg <- recv sock 50
-          putStrLn $ printHex msg
-          print "Received Message"
-          print (decode msg :: MessageClient)
-          nmsg <- next sock (message (decode msg :: MessageClient))
-          putStrLn . show $ C.length msg
-          putStrLn $ printHex msg
-          finalstep (message (decode nmsg :: MessageClient))
-          sClose sock
-          print "closed socket"
+          putStrLn $ show $ msg 
+
+            
+--           let tversionMessPayload = encode tversionClientMessage
+--           putStrLn . show $ C.length tversionMessPayload
+--           putStrLn $ printHex tversionMessPayload
+--           send sock (tversionMessPayload) >>= print . ("Sent: " ++) . show
+--           print "Tversion Message sent, receiving response"
+--           msg <- recv sock 50
+--           putStrLn $ printHex msg
+--           print "Received Message"
+--           print (decode msg :: MessageClient)
+--           nmsg <- next sock (message (decode msg :: MessageClient))
+--           putStrLn . show $ C.length msg
+--           putStrLn $ printHex msg
+--           finalstep (message (decode nmsg :: MessageClient))
+--           sClose sock
+--           print "closed socket"
