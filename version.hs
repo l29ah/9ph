34358976fcce81e649a0f424e146f065e830021d
dave <dave@mt64.local>
2010-02-08 22:35:58 -0800

    Added a test function for setting up work with ghci.  Successfully tested version, open and read so far.

--- version.hs
+++ version.hs
@@ -13,6 +13,24 @@ import Data.Binary.Builder
 import Debug.Trace
 import NineP
 
+connector :: IO Socket 
+connector = withSocketsDo $
+            do
+              ainfo <- getAddrInfo Nothing (Just "127.0.0.1") (Just "6872")
+              let a = head ainfo
+              sock <- socket AF_INET Stream defaultProtocol
+              putStrLn "Trying to connect"
+              connect sock (addrAddress (traceShow a a))
+              putStrLn "connected!"
+              let version = Msg TTversion (-1) $ Tversion 1024 "9P2000"
+              putStrLn $ "About to send: " ++ show version
+              send sock $ runPut (put version) 
+              putStrLn "Getting response"
+              msg <- recv sock 50
+              let response = runGet get msg ::Msg
+              putStrLn $ show response
+              return sock
+
 main :: IO ()
 main = withSocketsDo $
        do 
