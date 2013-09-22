1b9b5be65cf63ce8603058e6455b25b462c8bb38
Sergey Alirzaev <zl29ah@gmail.com>
2013-09-23 02:20:15 +0400

    make it compile against ghc-7.6.3

--- src/Data/NineP.hs
+++ src/Data/NineP.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      : Data.NineP
